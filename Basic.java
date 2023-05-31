302ed31249bfce2b2d907d4917d5facd3c9e87ac
Kartikbaghele <132550931+Kartikbaghele@users.noreply.github.com>
2023-05-31 09:31:35 +0530

    Add files via upload

---
+++ Basic.java
@@ -0,0 +1,23 @@
+package Vtigercrm;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class Basic {
+	public static void main(String[] args) throws InterruptedException {
+		WebDriver driver =new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://demo.vtiger.com/vtigercrm/");
+	   driver.findElement(By.xpath("//button[text()='Sign in']")).click();
+	   Thread.sleep(2000);
+	   driver.findElement(By.xpath("//span[@class='app-icon fa fa-bars']")).click();
+	   WebElement marketing = driver.findElement(By.xpath("//span[text()='MARKETING']"));
+	   Actions a=new Actions(driver);
+	   a.moveToElement(marketing).perform();
+	   driver.findElement(By.xpath("//span[text()=' Organizations']")).click();
+	}
+
+}
